969dbcd368fbabbe97dec5d97794b935b50ef393
MJ12X <179878352+MJ12X@users.noreply.github.com>
2024-10-15 02:35:34 +0100

    added to the Board class to display the board with vertical and horizontal lines.

--- main.rb
+++ main.rb
@@ -4,3 +4,10 @@ class Board
     @grid = Array.new(3) {Array.new(3, "")}
     @turns = 0
   end
+
+  def grid_display
+    grid.each_with_index do |row,index|
+    puts row.join("|")
+    puts "-------" unless index == grid.length - 1
+    end
+  end
